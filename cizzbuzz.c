031b7e50135b43ed19e258f37669148b60650165
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-10-13 14:19:12 +0200

    Add 🐛

--- cizzbuzz.c
+++ cizzbuzz.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  FILE* f = open("out.log", "r");
-  for(int i = 0; i <= 15; ++i) {
+FILE* open(const char*, const char*);
+
+int main(int argc, char** argv) {
+  FILE* f = open(argv[0], "w");
+  for(int i = argv[1]; i <= argv[2]; ++i) {
     if (i % 3 == 0) {
       printf("Fizz");
       fprintf(f, "Fizz");
@@ -16,12 +19,13 @@ int main() {
       fprintf(f, "%d", i);
     }
     printf("\n");
+    fprintf(f, "\n");
   }
   return 0;
 }
 
 FILE* open(const char* path, const char* mode) {
-  fopen(path, mode);
+  return fopen(path, mode);
 }
 
-// Hint: Check out https://man7.org/linux/man-pages/man3/fopen.3.html
+// Hint: Check out https://man7.org/linux/man-pages/man3/atoi.3.html
